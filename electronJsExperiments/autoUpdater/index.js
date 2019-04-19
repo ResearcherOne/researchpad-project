4b7412e1e062d94f787b601f66735e3ed9ee1d73
Andrew Camps <andrewcamps@dhcp-10-142-150-120.uawifi.arizona.edu>
2019-04-19 09:44:37 -0700

    testing github webhooks on ec2 instance for darwin releases

--- electronJsExperiments/autoUpdater/index.js
+++ electronJsExperiments/autoUpdater/index.js
@@ -7,6 +7,11 @@ const env_config = config[env];
 
 // Server routes
 app.get("/updates/latest", latestUpdateRoute);
+app.post("/release/darwin", darwinReleaseRoute);
+app.post("/release/win32", win32ReleaseRoute);
+app.use(function(req, res) {
+  res.status(404).send("404 path not found");
+});
 
 // Start server
 app.listen(env_config.port, function() {
@@ -22,4 +27,22 @@ function latestUpdateRoute(req, res) {
   res.json({
     url: ""
   });
+}
+
+/**
+ *
+ */
+function darwinReleaseRoute(req, res) {
+  console.log(req.data);
+
+  res.send("darwin release route");
+}
+
+/**
+ *
+ */
+function win32ReleaseRoute(req, res) {
+  console.log(req.data);
+
+  res.send("win32 release route");
 }

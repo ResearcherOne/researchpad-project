b0aab1bde891be45c28439cc135a09cf9d99c0e8
Andrew Camps <andrewcamps@dhcp-10-142-181-203.uawifi.arizona.edu>
2019-05-06 06:01:08 -0700

    trying to fix folder name

--- experiments/01-PDF-parsing-experiments/04-get-list-of-files-in-dictionary.js
+++
@@ -1,9 +0,0 @@
-const testFolder = './input/papers/';
-const fs = require('fs');
-
-var fileList = [];
-fs.readdirSync(testFolder).forEach(file => {
-  fileList.push(file);
-})
-
-console.log(fileList);
